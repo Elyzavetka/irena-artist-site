e6586900ee9affcce7ddb399c0e5c11c9fe41f77
Liza Tarasova <tarasova.liza.vic@gmail.com>
2025-04-09 22:14:18 +0100

    feat: add header styles to center header at the top

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+const Home: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.header}>Irena Yastremska</h1>
+    </div>
+  );
+};
+
+export default Home;
