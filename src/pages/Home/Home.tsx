ec113706d1c520814e54d11e7f500af7758c8f60
Liza Tarasova <tarasova.liza.vic@gmail.com>
2025-04-14 23:55:41 +0100

    feat(ui): add responsive header and full-width image

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./Home.module.css";
+import pecherskImg from "../../assets/pechersk_administration.jpg";
 
 const Home: React.FC = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>Irena Yastremska</h1>
+      <img
+        src={pecherskImg}
+        alt="Pechersk Administration"
+        className={styles.pecherskImg}
+      />
     </div>
   );
 };
